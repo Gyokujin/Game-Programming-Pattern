aa69a73ea8036d8d2e36b78169572e963e974817
Gyokujin <dd43454548@naver.com>
2024-11-21 18:18:11 +0900

    # 팩토리 패턴 수정

--- Assets/Scripts/2DesgnPattern/2FactoryPattern/CafeMenu/CafeLatte.cs
+++ Assets/Scripts/2DesgnPattern/2FactoryPattern/CafeMenu/CafeLatte.cs
@@ -1,7 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
 public class CafeLatte : CafeMenu
 {
     public override void Init()
--- Assets/Scripts/2DesgnPattern/2FactoryPattern/CafeMenu/Coffee.cs
+++ Assets/Scripts/2DesgnPattern/2FactoryPattern/CafeMenu/Coffee.cs
@@ -1,7 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
 public class Coffee : CafeMenu
 {
     public override void Init()
